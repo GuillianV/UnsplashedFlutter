fa5c233e264d4f23ec10c6e29d05467e9248656c
GuillianV <guillianvibert90@gmail.com>
2021-11-19 14:31:57 +0100

    add infinite scroll and blurHash

--- lib/models/photo.dart
+++ lib/models/photo.dart
@@ -7,22 +7,27 @@ class Photo {
   String? description;
   String? regularUrl;
   String fullUrl;
+  String smallUrl;
   String? rawUrl; //For downloading image only
   String? userName; //Attribution to the photographer
   String? userProfileUrl; //Photographer's profile
   String? userProfileImage; //Photographer's profile image
   int? likes;
-  String? blurHash; //Optional
+  String blurHash; //Optional
   String? downloadLocation; //Optional
   Color? color; //Optional
 
-  Photo(this.id, this.fullUrl);
+  Photo(this.id, this.fullUrl, this.smallUrl, this.blurHash);
 
   factory Photo.fromJson(Map<String, dynamic> json) {
-    return Photo(json["id"], json["urls"]["full"]);
+    return Photo(json["id"], json["urls"]["full"], json["urls"]["small"],
+        json["blur_hash"]);
   }
 }
 
 List<Photo> allFromJson(List<dynamic> list) {
-  return list.map((item) => Photo(item["id"], item["urls"]["full"])).toList();
+  return list
+      .map((item) => Photo(item["id"], item["urls"]["full"],
+          item["urls"]["small"], item["blur_hash"]))
+      .toList();
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:unsplashed/models/photo.dart';
 import 'package:unsplashed/models/http';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -10,35 +12,83 @@ class MainPage extends StatefulWidget {
 }
 
 class MainPageState extends State<MainPage> {
+  late int _pageNumber;
+  late bool _error;
+  late bool _loading;
+  late final int _defaultPhotosPerPageCount = 10;
+  List<Photo> _photos = [];
+  final int _nextPageThreshold = 5;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageNumber = 1;
+    _error = false;
+    _loading = true;
+    _photos = [];
+    fetchPhotos();
+  }
+
+  Future<void> fetchPhotos() async {
+    try {
+      final fetchedPhotos = await http_get_Photos(pageNumber: _pageNumber);
+      setState(
+        () {
+          _loading = false;
+          _pageNumber = _pageNumber + 1;
+          _photos.addAll(fetchedPhotos);
+        },
+      );
+    } catch (e) {
+      setState(
+        () {
+          _loading = false;
+          _error = true;
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Center(
-          child: FutureBuilder<List<Photo>>(
-        future: http_get_Photos(), // async work
-        builder: (BuildContext context, AsyncSnapshot<List<Photo>> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return const Center(
-                child: CircularProgressIndicator(color: Colors.white),
-              );
-            default:
-              if (snapshot.hasError && snapshot.data != null)
-                return Text('Error');
-              else {
-                return GridView.count(
-                    // Create a grid with 2 columns. If you change the scrollDirection to
-                    // horizontal, this produces 2 rows.
-                    crossAxisCount: 2,
-                    // Generate 100 widgets that display their index in the List.
+          child: GridView.builder(
+              itemCount: _photos.length,
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this produces 2 rows.
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              // Generate 100 widgets that display their index in the List.
 
-                    children: snapshot.data!
-                        .map((photo) => Image.network(photo.fullUrl))
-                        .toList());
-              }
-          }
-        },
-      )),
+              itemBuilder: (context, index) {
+                if (index == _photos.length - _nextPageThreshold) {
+                  fetchPhotos();
+                }
+                final Photo photo = _photos[index];
+
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Center(
+                      child: CachedNetworkImage(
+                        fit: BoxFit.cover,
+                        width: width,
+                        height: height,
+                        imageUrl: photo.fullUrl,
+                        placeholder: (context, url) =>
+                            BlurHash(hash: photo.blurHash),
+                        errorWidget: (context, url, error) =>
+                            new Icon(Icons.error),
+                      ),
+                    ),
+                  ),
+                );
+              })),
     );
   }
 }
